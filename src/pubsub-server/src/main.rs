87e5c43f73633519b28d85f042567ca6fff3a462
Joaquim Monteiro <joaquim.monteiro@protonmail.com>
2022-10-14 23:29:36 +0100

    Parse command line arguments in the server

--- src/pubsub-server/src/main.rs
+++ src/pubsub-server/src/main.rs
@@ -1,1 +1,12 @@
-fn main() {}
+use clap::Parser;
+
+#[derive(Parser)]
+struct Args {
+    #[arg(short, long, default_value_t = 5555)]
+    port: u16,
+}
+
+fn main() -> Result<(), zmq::Error> {
+    let args = Args::parse();
+    Ok(())
+}
