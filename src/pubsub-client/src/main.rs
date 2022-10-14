096ba69c29d8b5a5871b48950ee4295b4258b65d
Joaquim Monteiro <joaquim.monteiro@protonmail.com>
2022-10-14 23:27:26 +0100

    Parse command line arguments in the client

--- src/pubsub-client/src/main.rs
+++ src/pubsub-client/src/main.rs
@@ -1,1 +1,22 @@
-fn main() {}
+use clap::Parser;
+
+#[derive(clap::Parser, Debug)]
+struct Args {
+    #[command(subcommand)]
+    mode: Mode,
+    #[arg(short, long, default_value_t = 5555)]
+    port: u16,
+}
+
+#[derive(clap::Subcommand, Debug)]
+enum Mode {
+    #[command(name = "pub")]
+    Publisher,
+    #[command(name = "sub")]
+    Subscriber,
+}
+
+fn main() {
+    let args = Args::parse();
+    println!("{:#?}", args);
+}
